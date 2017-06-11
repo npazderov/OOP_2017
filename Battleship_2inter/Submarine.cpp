0f894c68ef03d765dca9804cd3fe53582084f159
npazderov <nikola_pazderov@abv.bg>
2017-06-11 19:10:33 +0300

    Create Submarine.cpp

--- Battleship_2inter/Submarine.cpp
+++ Battleship_2inter/Submarine.cpp
@@ -5,19 +5,16 @@ Submarine::Submarine()
     //ctor
 }
 
-Submarine::~Submarine()
-{
-    //dtor
-}
-
 Submarine::Submarine(const Submarine& other)
+    :Ship(other)
 {
-    //copy ctor
 }
 
 Submarine& Submarine::operator=(const Submarine& rhs)
 {
-    if (this == &rhs) return *this; // handle self assignment
-    //assignment operator
+    if (this == &rhs)
+    {
+        Ship::operator=(rhs);
+    }
     return *this;
 }
