c6f4de4b1bdb12928c6b316f3d2011c02ac4878e
npazderov <nikola_pazderov@abv.bg>
2017-06-11 19:59:50 +0300

    Create Ship.cpp

--- Battleship_2inter/Ship.cpp
+++ Battleship_2inter/Ship.cpp
@@ -2,45 +2,145 @@
 
 Ship::Ship()
 {
-    Sethp(0);
-    Setsize(0);
+    hp = 0;
+    ID = '.';
+    positionX = 0;
+    positionY = 0;
 }
-Ship::Ship(int newHp, int newSize)
+Ship::Ship( int newHp, char newID, int newPositionX, int newPositionY)
 {
-    Sethp(newHp);
-    Setsize(newSize);
+    hp = newHp;
+    ID = newID;
+    positionX = newPositionX;
+    positionY = newPositionY;
 }
 
 Ship::Ship(const Ship& other)
 {
-    Sethp(other.hp);
-    Setsize(other.size);
+    hp = other.hp;
+    ID = other.ID;
+    positionX = other.positionX;
+    positionY = other.positionY;
 }
 
 Ship& Ship::operator=(const Ship& rhs)
 {
-    if (this == &rhs)
+    if (this == &rhs) 
     {
-        Sethp(rhs.hp);
-        Setsize(rhs.size);
+    hp = rhs.hp;
+    ID = rhs.ID;
+    positionX = rhs.positionX;
+    positionY = rhs.positionY;
     }
     return *this;
+
 }
-//Getters
-Ship::Gethp() const
+int Ship::getHp() const
 {
     return hp;
 }
-Ship::Getsize() const
+char Ship::getID()const
 {
-    return size;
+    return ID;
 }
-//Setters
-void Ship::Sethp(int newHp)
+int Ship::getPositionX() const
 {
-    hp=newHp;
+    return positionX;
 }
-void Ship::Setsize(int newSize)
+
+int Ship::getPositionY() const
 {
-    size=newSize;
+    return positionY;
+}
+void Ship::setHP(int _HP)
+{
+    hp = _HP;
+}
+
+void Ship::setID(char _ID)
+{
+    ID = _ID;
+}
+
+void Ship::place(int _positionX, int _positionY)
+{
+    char userOrientation;
+    if ((_positionX >= 0 && _positionX <= 7) && (_positionY >= 0 && _positionY <= 7))
+    {
+        do
+        {
+            cout << "Enter U / D / L / R for orientation of the Ship:";
+            cin >> userOrientation;
+            if (userOrientation == 'U')
+            {
+                if (_positionY >= this->getHp())
+                {
+                    if(collisionU(_positionX, _positionY, this->getHp() == false))
+                       {
+                        for (size_t i = _positionY; i < this->getHp(); i++)
+                            {
+                                setAt(_positionX, _positionY - i, this->getID());
+                            }
+                       }
+                    else
+                    {
+                        cout << "There is a ship on one of the positions. Try again..." << endl;
+                    }
+                }
+            }
+            if (userOrientation == 'D')
+            {
+                if (_positionY <= this->getHp())
+                {
+                    if(collisionD(_positionX, _positionY, this->getHp() == false))
+                       {
+                        for (size_t i = _positionY; i < this->getHp(); i++)
+                            {
+                                setAt(_positionX, _positionY + i, this->getID());
+                            }
+                       }
+                    else
+                    {
+                        cout << "There is a ship on one of the positions. Try again..." << endl;
+                    }
+                }
+            }
+            if (userOrientation == 'L')
+            {
+                if (_positionX >= this->getHp())
+                {
+                    if(collisionL(_positionX, _positionY, this->getHp() == false))
+                       {
+                        for (size_t i = _positionX; i < this->getHp(); i++)
+                            {
+                                setAt(_positionX - i, _positionY, this->getID());
+                            }
+                       }
+                    else
+                    {
+                        cout << "There is a ship on one of the positions. Try again..." << endl;
+                    }
+                }
+            }
+            if (userOrientation == 'R')
+            {
+                if (_positionX <= this->getHp())
+                {
+                    if(collisionR(_positionX, _positionY, this->getHp() == false))
+                       {
+                        for (size_t i = _positionX; i < this->getHp(); i++)
+                            {
+                                setAt(_positionX + i, _positionY, this->getID());
+                            }
+                       }
+                    else
+                    {
+                        cout << "There is a ship on one of the positions. Try again..." << endl;
+                    }
+                }
+            }
+            break;
+        }
+        while (true);
+    }
 }
