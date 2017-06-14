2fe6b57ee07d4cd81d514f504d323a71902d5bfd
npazderov <nikola_pazderov@abv.bg>
2017-06-14 20:50:17 +0300

    Add files via upload

---
+++ Battleship_2inter/position.cpp
@@ -0,0 +1,10 @@
+#include "position.h"
+
+ShipPosition::position()
+{
+    this ->x1 = 0;
+    this ->x2 = 0;
+    this ->y1 = 0;
+    this ->y2 = 0;
+}
+
---
+++ Battleship_2inter/position.h
@@ -0,0 +1,11 @@
+#pragma once
+
+struct ShipPosition
+{
+    int x1;
+    int x2;
+    int y1;
+    int y2;
+
+    position();
+};
