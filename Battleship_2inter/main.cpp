e99296db6a8cd5aac89608e70bc65a4b37d02226
npazderov <nikola_pazderov@abv.bg>
2017-06-14 21:03:08 +0300

    Create main.cpp

--- Battleship_2inter/main.cpp
+++ Battleship_2inter/main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 
+#include "map.h"
+#include "Battleship.h"
+#include "Carrier.h"
+#include "Cruiser.h"
+#include "Submarine.h"
+#include "Destroyer.h"
+#include "Ship.h"
 using namespace std;
-
+int Player1_HP = 15;
+int Player2_HP = 15;
 int main()
 {
-    cout << "Hello world!" << endl;
+    Map A;
+    Map B;
+    Map hidden_A;
+    Map hidden_B;
+    Cruiser R1;
+    Battleship B1;
+    R1.place(A);
+    A.printField();
+    B1.place(A);
+    A.printField();
+
+    Cruiser R2;
+    Battleship B2;
+    R2.place(B);
+    B2.place(B);
+
+    A.printMap();
+    cout<<endl;
+    B.printMap();
+
+    R1.attack(B, hidden_B, Player2_HP);
+    hidden_B.printMap();
+
     return 0;
 }
