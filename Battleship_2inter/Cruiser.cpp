7e54881928492fd59a420391dd1a1ad3423e948e
npazderov <nikola_pazderov@abv.bg>
2017-06-14 21:00:48 +0300

    Create Cruiser.cpp

--- Battleship_2inter/Cruiser.cpp
+++ Battleship_2inter/Cruiser.cpp
@@ -5,6 +5,14 @@ Cruiser::Cruiser()
     setHP(3);
     setID('Cr');
 }
+Cruiser::Cruiser(int newHp,char newID, int newPositionX, int newPositionY):Ship(newHp, newID, newPositionX, newPositionY)
+
+{
+    hp = newHp;
+    ID = newID;
+    positionX = newPositionX;
+    positionY = newPositionY;
+}
 
 Cruiser::Cruiser(const Cruiser& other)
    : Ship(other)
